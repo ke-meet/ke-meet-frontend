f72e647c0b5f85e86e10e899b43646f28a3fcd75
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-07-04 00:38:01 +0900

    NavBar

---
+++ ke-meet/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import GlobalStyle from './GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Routers />
+    </>
+  );
+}
+
+export default App;
